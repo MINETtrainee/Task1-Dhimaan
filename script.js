d70d605d21af6c9b33e877a79480aaa91bf3c96f
Dhimaan Ayan <dhimaanayan@Dhimaans-MacBook-Pro.local>
2022-06-07 22:43:24 +0530

    intial

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 'use srict';
 
-const tool = prompt('What calculation do you want to do? Pick between addition, subtraction, multiplication, division, to the power , square root, sine, cos, tan, logN');
-const toolS = tolowerCase(tool);
+const tool = prompt('What calculation do you want to do? Pick between addition, subtraction, multiplication, division, to the power , square root, sine, cos, tan, logn (all small)');
 
 
 let n1 = prompt(`What is your first number, ____   ${tool}  -----`);
 let n2;  
 
 
-if(tool != 'square root' && tool != 'sin' && tool != 'cos' && tool != 'tan' && tool != 'log'){
+if(tool != 'square root' && tool != 'sin' && tool != 'cos' && tool != 'tan' && tool != 'logn'){
   n2 = prompt(`What is your second number, ------ ${tool}________`)
 }
 
 
-if(toolS === 'addition') {
+if(tool === 'addition') {
   alert(Number(n1) + Number(n2));
-} else if (toolS === 'subtraction') {
+} else if (tool === 'subtraction') {
   alert(Number(n1) - Number(n2));
-} else if (toolS === 'multiplication') {
+} else if (tool === 'multiplication') {
   alert(Number(n1) * Number(n2));
-} else if (toolS === 'division') {
+} else if (tool === 'division') {
   alert(Number(n1) / Number(n2));
-} else if (toolS === 'to the power') {
+} else if (tool === 'to the power') {
   alert(Number(n1) ** Number(n2));
-} else if (toolS === 'square root') {
+} else if (tool === 'square root') {
   alert(Math.sqrt(Number(n1)))
-} else if (toolS === 'sine'){
+} else if (tool === 'sine'){
   alert(Math.sin(Number(n1)))
-}else if (toolS === 'cos'){
+}else if (tool === 'cos'){
   alert(Math.cos(Number(n1)))
-} else if (toolS === 'tan') {
+} else if (tool === 'tan') {
   alert(Math.tan(Number(n1)))
-} else if (toolS === 'logn'){
+} else if (tool === 'logn'){
   alert(Math.log(Number(n1)))
 }else {
   alert ('Invalid !')
