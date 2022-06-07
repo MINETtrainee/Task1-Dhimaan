f28eed908c723c0179086e57b1a47f70f47238c3
Dhimaan Ayan <dhimaanayan@Dhimaans-MacBook-Pro.local>
2022-06-07 22:05:22 +0530

    intial

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 'use srict';
 
-let tool = prompt('What calculation do you want to do? Pick between addition, subtraction, multiplication, division, to the power (case sensetive)');
+let tool = prompt('What calculation do you want to do? Pick between addition, subtraction, multiplication, division, to the power , square root, sin , cos, tan, log (case sensetive all small)');
 
 let n1 = prompt(`What is your first number, ____   ${tool}  -----`);
-let n2 = prompt(`What is your second number, ------ ${tool}________`)
+let n2;  
+
+
+if(tool != 'square root' && tool != 'sin' && tool != 'cos' && tool != 'tan' && tool != 'log'){
+  prompt(`What is your second number, ------ ${tool}________`)
+}
+
 
 if(tool === 'addition') {
   alert(Number(n1) + Number(n2));
@@ -15,7 +21,16 @@ if(tool === 'addition') {
   alert(Number(n1) / Number(n2));
 } else if (tool === 'to the power') {
   alert(Number(n1) ** Number(n2));
-} else {
+} else if (tool === 'square root') {
+  alert(Math.sqrt(Number(n1)))
+} else if (tool === 'sin'){
+  alert(Math.sin(Number(n1)))
+}else if (tool === 'cos'){
+  alert(Math.cos(Number(n1)))
+} else if (tool === 'tan') {
+  alert(Math.tan(Number(n1)))
+} else if (tool === 'log'){
+  alert(Math.log(Number(n1)))
+}else {
   alert ('Invalid !')
 }
-
